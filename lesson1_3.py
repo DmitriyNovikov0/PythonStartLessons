fbd0b6d347c33f31008337b99f4a7ef218b480b3
Dmitriy Novikov <adin@mail.ru>
2022-07-13 12:52:56 +0600

    дз3

--- lesson1_3.py
+++
@@ -1,3 +0,0 @@
-s_nmb = input('Введите число: ')
-# не правильно это без try catch делать, но мы пока не знаем об исключениях :)))))
-print(f'{s_nmb} + {s_nmb + s_nmb} + {s_nmb + s_nmb + s_nmb} = {int(s_nmb) + int(s_nmb + s_nmb) + int(s_nmb + s_nmb + s_nmb)}')
