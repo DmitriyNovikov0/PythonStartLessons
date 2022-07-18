b2603c0ff92a4be1c48f74ed016ccd87d7586f08
Dmitriy Novikov <adin@mail.ru>
2022-07-18 20:19:34 +0600

    дз3 сдано

--- Lesson3/Task6.py
+++ Lesson3/Task6.py
@@ -5,5 +5,4 @@ def my_func(in_str):
     tmp_list = in_str.split(' ')
     return ' '.join( map(int_func, tmp_list))
 
-
 print( my_func('lorem ipsum is simply dummy text of the printing and typesetting industry') )
